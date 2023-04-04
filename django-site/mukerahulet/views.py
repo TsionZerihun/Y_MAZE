f18cb1c955e0d90185b1ecbee1ffb1a69ab97826
TsiLidu <122775151+TsiLidu@users.noreply.github.com>
2023-04-04 08:19:04 +0300

    Update views.py

--- django-site/mukerahulet/views.py
+++ django-site/mukerahulet/views.py
@@ -2,8 +2,4 @@ from django.shortcuts import render
 from utils.util import calcualte_maze_points
 def home(request):
     sap, sar, aar, percentage=calcualte_maze_points()
-    #sap = [['C', 'C'], ['C', 'C']]
-    #sar = [['C', 'C'], ['C', 'C'], ['C', 'C'], ['C', 'C'], ['A', 'A'], ['C', 'C']]
-    #aar = [['A', 'C', 'C'], ['C', 'C', 'A'], ['C', 'A', 'A'], ['A', 'A', 'C'], ['A', 'C', 'A'], ['B', 'C', 'C'], ['C', 'C', 'A'], ['C', 'A', 'C']]
-
     return render(request, 'bkp.html',{'sap':sap, 'sar':sar, 'aar':aar})
